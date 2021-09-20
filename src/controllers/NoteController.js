0e49d3756f994c36b061149aac1ab70e8cc2dfd7
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-09-20 10:58:00 -0300

    refactor(Note): refactored all roots functions for Node type to another module

---
+++ src/controllers/NoteController.js
@@ -0,0 +1,41 @@
+import {
+  updateNoteTitle as updNoteTitle,
+  updateNoteBody as updNoteBody,
+  getNote as gNote,
+  getUserNotes as gNotes,
+  pushNote,
+} from '../models/User';
+
+export const insertNote = (data) => {
+  const { user_id, body, title } = data;
+
+  return user_id && body && title ? pushNote(user_id, body, title) : null;
+};
+
+export const updateNoteTitle = (data) => {
+  const { user_id, note_id, title } = data;
+
+  return user_id && note_id && title
+    ? updNoteTitle(user_id, note_id, title)
+    : null;
+};
+
+export const updateNoteBody = (data) => {
+  const { user_id, note_id, body } = data;
+
+  return user_id && note_id && body
+    ? updNoteBody(user_id, note_id, body)
+    : null;
+};
+
+export const getNote = (data) => {
+  const { user_id, note_id } = data;
+
+  return user_id && note_id ? gNote(user_id, note_id) : null;
+};
+
+export const getNotes = (data) => {
+  const { user_id } = data;
+
+  return user_id ? gNotes(user_id) : null;
+};
