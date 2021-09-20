40cc0d80f4388c5e8f781ff1d3940c33a215a8c5
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-09-20 11:40:38 -0300

    fix(NoteController): Specifying user_id as Number

--- src/controllers/NoteController.js
+++ src/controllers/NoteController.js
@@ -9,14 +9,16 @@ import {
 export const insertNote = (data) => {
   const { user_id, body, title } = data;
 
-  return user_id && body && title ? pushNote(user_id, body, title) : null;
+  return user_id && body && title
+    ? pushNote(Number(user_id), body, title)
+    : null;
 };
 
 export const updateNoteTitle = (data) => {
   const { user_id, note_id, title } = data;
 
   return user_id && note_id && title
-    ? updNoteTitle(user_id, note_id, title)
+    ? updNoteTitle(Number(user_id), note_id, title)
     : null;
 };
 
@@ -24,18 +26,18 @@ export const updateNoteBody = (data) => {
   const { user_id, note_id, body } = data;
 
   return user_id && note_id && body
-    ? updNoteBody(user_id, note_id, body)
+    ? updNoteBody(Number(user_id), note_id, body)
     : null;
 };
 
 export const getNote = (data) => {
   const { user_id, note_id } = data;
 
-  return user_id && note_id ? gNote(user_id, note_id) : null;
+  return user_id && note_id ? gNote(Number(user_id), note_id) : null;
 };
 
 export const getNotes = (data) => {
   const { user_id } = data;
 
-  return user_id ? gNotes(user_id) : null;
+  return user_id ? gNotes(Number(user_id)) : null;
 };
