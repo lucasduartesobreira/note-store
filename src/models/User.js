aa942fe0e55c9d6cfa9bf19536f0c695a2b93420
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-09-20 11:38:44 -0300

    feat(User): Added updates to the Note

--- src/models/User.js
+++ src/models/User.js
@@ -19,3 +19,21 @@ export const insertNote = (user_id, body, title) => {
   return null;
 };
 
+export const updateNoteTitle = (user_id, note_id, title) => {
+  if (user_bd.has(user_id)) {
+    const user = user_bd.get(user_id);
+    user.notes.get(note_id).title = title;
+    return user.notes.get(note_id);
+  }
+  return null;
+};
+
+export const updateNoteBody = (user_id, note_id, body) => {
+  if (user_bd.has(user_id)) {
+    const user = user_bd.get(user_id);
+    user.notes.get(note_id).body = body;
+    return user.notes.get(note_id);
+  }
+  return null;
+};
+
