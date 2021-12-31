845041bada4a41a4cd06fef922b8a6b2ae877f44
felipeLeao18 <flpleao1807@gmail.com>
2021-12-31 19:52:59 -0300

    update: controller syntax to accept user as entity to call methods

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,11 +1,11 @@
-import { createUser as cUser, deleteUser as dUser } from '../models/User.js';
+import { user} from '../models/User.js';
 
 export const createUser = (user_bd) => () => {
-  return cUser(user_bd);
+  return user.create(user_bd);
 };
 
 export const deleteUser = (user_bd) => (data) => {
   const { user_id } = data;
 
-  return user_id ? dUser(user_bd, Number(user_id)) : null;
+  return user_id ? user.remove(user_bd, Number(user_id)) : null;
 };
